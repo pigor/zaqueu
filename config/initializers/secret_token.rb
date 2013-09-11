4ce5f72429b1c94a2219f09e71618de7acfe318f
Carlos Souza <carloshrsouza@gmail.com>
2013-09-11 14:27:02 -0400

    Protegendo valor do secret_key_base.
    Em development, setar ENV['SECRET_KEY_BASE'] antes
    de rodar o server. Rodar `rake secret` para obter um valor.
    
    fixes #4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Zaqueu::Application.config.secret_key_base = 'b5cf7bc1e07ca54ef4a356bcf40de6083c6e67dbb858e14722923a775f8c272d3b524bb2a07cbf2dbffd9e8b7a68b4cc4aa8d5265d96b070f7905f59f788cd67'
+Zaqueu::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
