04aeee23aa91fdef0295b5fbde9345a81b12fac5
Paulo Igor <piagodinho@gmail.com>
2013-09-26 15:22:14 -0300

    Adicionando Clockwork para disparar os emails automaticamente

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: bundle exec passenger start -p $PORT --max-pool-size 3
+clock: bundle exec clockwork lib/clock.rb
--- app/models/collection.rb
+++ app/models/collection.rb
@@ -2,4 +2,12 @@ class Collection < ActiveRecord::Base
   belongs_to :user
 
   validates :description, :deadline, :email, presence: true
+
+  scope :by_deadline, ->(datetime) { where(deadline: datetime.strftime("%Y-%m-%d %H:%M")) }
+
+  def notify
+    Notification.notify(self).deliver
+
+    self.update_attributes(send_count: (self.send_count + 1))
+  end
 end
