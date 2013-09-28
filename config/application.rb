385b900db9c0860f0b6741a17b0a48995a2950f6
Paulo Igor <piagodinho@gmail.com>
2013-09-28 15:57:41 -0300

    Ajustando timezone

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Zaqueu
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Brasilia'
+    config.time_zone = 'Brasilia'
     config.active_record.default_timezone = :local
     config.active_record.time_zone_aware_attributes = false
 
