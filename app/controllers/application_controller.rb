d80a105b71897c8138cd65a74a6bdd46db10783e
Paulo Igor <piagodinho@gmail.com>
2013-10-02 16:50:18 -0300

    Traduzindo a mensagem que exige o login para acessar o sistema

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   private
   
   def not_authenticated
-    redirect_to login_url, :alert => "First log in to view this page."
+    redirect_to login_url, notice: "Faça o login para acessar a sua página."
   end
 end
