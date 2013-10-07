b6bb6b15c895c6d1ff156af3d4cd74d3ef5992fd
Carlos Souza <carloshrsouza@gmail.com>
2013-10-07 17:47:55 -0400

    Remove formatação desnecessária.

--- test/models/collection_test.rb
+++ test/models/collection_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class CollectionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @user ||= User.create(username: 'foo', name: 'Foo', email: 'bar@foo', password: '123')
+    @collection ||= Collection.create(description: 'Yay',
+                                  deadline: 5.days.from_now,
+                                  email: 'foo@bar.com', user: @user)
+  end
+
+  test '.by_deadline retorna itens vencendo até 10 minutos antes da deadline' do
+    assert_empty Collection.by_deadline(1.day.from_now)
+    assert_includes Collection.by_deadline(5.days.from_now), @collection
+    assert_includes Collection.by_deadline(5.days.from_now), @collection
+  end
 end
